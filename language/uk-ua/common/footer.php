a868497869d92d1429579b3df9ab4269a349b1a4
MAXIM-SYSOEV\WorkPC <work@maks4a.gmail>
2020-02-24 09:19:38 +0200

    init

---
+++ language/uk-ua/common/footer.php
@@ -0,0 +1,3 @@
+<?php
+$_['text_powered']      = '%s <a href="http://www.documentov.com" target="_blank">%s</a> ';
+$_['text_powered_addt'] = ' На архитектуре <a href="http://www.opencart.com" target="_blank">OpenCart</a>';
---
+++ language/uk-ua/document/folder.php
@@ -0,0 +1,18 @@
+<?php
+
+$_['text_title_filter']         = 'Фильтр';
+
+$_['column_filter_field']       = 'Поле';
+$_['column_filter_condition']   = 'Сравнение';
+$_['column_filter_value']       = 'Значение';
+
+$_['button_filter']             = 'Фильтровать';
+$_['button_close']              = 'Закрыть';
+$_['button_reset']              = 'Сбросить';
+
+$_['entry_filter_name']         = 'Название фильтра';
+
+$_['text_total_documents']      = 'Всего: ';
+$_['text_show_documents']       = 'по: ';
+$_['text_ftsearch_invalid_input'] = 'В режиме полнотекстового поиска запрос должен содержать слова длиною не менее 4-х символов';
+$_['text_error_no_doctype']       = 'Ошибка: для журнала не выбран тип документа';
